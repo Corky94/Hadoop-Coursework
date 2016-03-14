ab35d023e96ddb43cbc8669cf425f866a2d8b633
Marc Cork <marc.cork@gmail.com>
2016-03-14 14:55:58 +0000

    I want to cry

--- src/main/java/MatrixMultiplication.java
+++ src/main/java/MatrixMultiplication.java
@@ -12,7 +12,9 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -30,10 +32,8 @@ public class MatrixMultiplication {
                 String[] itr = value.toString().split("\n");
                 String[] vectorSplit = vector.toString().split("\n");
                 for (String val : itr) {
-                    String [] splitLine = val.split(",");
-                    word.set(splitLine[0]);
-                    int value1 = Integer.parseInt(splitLine[2]) * Integer.getInteger(vectorSplit[Integer.getInteger(splitLine[1])].split(",")[2]);
-                    IntWritable v = new IntWritable(value1);
+
+                    IntWritable v = new IntWritable(1);
                     context.write(word,v);
                 }
 
@@ -41,6 +41,7 @@ public class MatrixMultiplication {
 
             public static void setVector(String vectorString) {
                 vector = vectorString;
+                System.out.println("\n\n\n\n" + vectorString + "\n\n\n\n");
             }
         }
 
@@ -64,7 +65,15 @@ public class MatrixMultiplication {
 
         public static void main(String[] args) throws Exception {
             Configuration conf = new Configuration();
-            TokenizerMapper.setVector(new File(args[3]).toString());
+            StringBuilder builder = new StringBuilder();
+
+            BufferedReader fileReader = new BufferedReader(new FileReader(new File(args[2])));
+            String line = fileReader.readLine();
+            while(line!= null){
+                builder.append(line);
+                line = fileReader.readLine();
+            }
+            TokenizerMapper.setVector(builder.toString());
             Job job = Job.getInstance(conf, "word count");
             job.setJarByClass(MatrixMultiplication.class);
             job.setMapperClass(TokenizerMapper.class);
