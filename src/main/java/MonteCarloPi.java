89215dea2601846d1bd3bf50ac44757283ea0a7f
Marc Cork <marc.cork@gmail.com>
2016-03-14 15:53:23 +0000

    Maybe I can do this first time =D

---
+++ src/main/java/MonteCarloPi.java
@@ -0,0 +1,96 @@
+/**
+ * Created by userhp on 14/03/2016.
+ */
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class MonteCarloPi {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+
+            String[] splitValue = value.toString().split(",");
+            int j = Integer.parseInt(splitValue[0]);
+            int N = Integer.parseInt(splitValue[1]);
+
+            int i;
+            double x, y;
+            for(i=0; i< j*N; i++){
+                //OFFSET Rand
+                x = Math.random();
+                y = Math.random();
+            }
+
+            for (i = 0; i < N; i++) {
+                x = Math.random()*4;
+                y = Math.random()*4;
+
+
+                if (x * x + y * y <= 4){
+                    word.set("1");
+
+                }
+                else{
+                    word.set("0");
+                }
+                context.write(word,one);
+            }
+
+        }
+    }
+
+
+        public static class IntSumReducer
+                extends Reducer<Text,IntWritable,Text,IntWritable> {
+            private IntWritable result = new IntWritable();
+
+            public void reduce(Text key, Iterable<IntWritable> values,
+                               Context context
+            ) throws IOException, InterruptedException {
+                int sum = 0;
+                for (IntWritable val : values) {
+                    sum += val.get();
+                }
+                result.set(sum);
+                context.write(key, result);
+            }
+        }
+
+
+        public static void throwDarts(int N,int offset){
+
+        }
+            public static void main(String[] args) throws Exception {
+                Configuration conf = new Configuration();
+                Job job = Job.getInstance(conf, "word count");
+                job.setJarByClass(WordCountOne.class);
+                job.setMapperClass(TokenizerMapper.class);
+                job.setReducerClass(IntSumReducer.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(IntWritable.class);
+                FileInputFormat.addInputPath(job, new Path(args[0]));
+                FileOutputFormat.setOutputPath(job, new Path(args[1]));
+                System.exit(job.waitForCompletion(true) ? 0 : 1);
+            }
+    }
+
