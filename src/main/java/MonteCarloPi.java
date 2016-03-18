0c25a979cde2e04aa99f4c1e3fa5c9c607f3294f
Marc <Marc.Cork@gmail.com>
2016-03-18 16:09:35 +0000

    Completed coursework with data and examples, to run need to have apache hadoop in the top directory

--- src/main/java/MonteCarloPi.java
+++ src/main/java/MonteCarloPi.java
@@ -85,6 +85,7 @@ public class MonteCarloPi {
                 Job job = Job.getInstance(conf, "word count");
                 job.setJarByClass(WordCountOne.class);
                 job.setMapperClass(TokenizerMapper.class);
+job.setCombinerClass(IntSumReducer.class);
                 job.setReducerClass(IntSumReducer.class);
                 job.setOutputKeyClass(Text.class);
                 job.setOutputValueClass(IntWritable.class);
